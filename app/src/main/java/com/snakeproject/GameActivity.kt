3cfd91df5123fa8253a6be4456b8d9fd5fbeecc6
uppercasePhi <thefilippxl@gmail.com>
2019-06-13 22:02:40 +0300

    Added in-game pause

--- app/src/main/java/com/snakeproject/GameActivity.kt
+++ app/src/main/java/com/snakeproject/GameActivity.kt
@@ -1,39 +1,55 @@
 package com.snakeproject
 
+import android.content.DialogInterface
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 
 class GameActivity : AppCompatActivity() {
 
-    var snakeGame: SnakeGame? = null
+    private lateinit var snakeGame: SnakeGame
+    private lateinit var pause: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Game paused")
+        builder.setNegativeButton("Resume") { _: DialogInterface, _: Int -> onResume() }
+        builder.setPositiveButton("Quit") { _: DialogInterface, _: Int -> finish() }
+
+        pause = builder.create()
+
         val level = intent.getIntExtra("level", 20)
-        var kitNumber = intent.getIntExtra("kit", 1)
+        val kitNumber = intent.getIntExtra("kit", 1)
 
         snakeGame = SnakeGame(this, level, kitNumber)
 
         setContentView(snakeGame)
 
-        snakeGame!!.deadCallback = ::onDeath
+        snakeGame.deadCallback = ::onDeath
     }
 
-    public fun onDeath() {
-        snakeGame!!.pauseGame()
+    override fun onBackPressed() {
+        onPause()
+        pause.show()
+    }
+
+    private fun onDeath() {
+        snakeGame.pauseGame()
         finish()
     }
 
     override fun onResume() {
         super.onResume()
-        snakeGame!!.post {
-            snakeGame!!.resumeGame()
+        snakeGame.post {
+            snakeGame.resumeGame()
         }
     }
 
     override fun onPause() {
         super.onPause()
-        snakeGame!!.pauseGame()
+        snakeGame.pauseGame()
     }
 }
